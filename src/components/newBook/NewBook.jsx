4664e2263ec366f77b7666e933dc8a17759ff55b
EstebanFerreyra <eferreyra098@gmail.com>
2024-06-07 17:12:06 -0300

    useReducer

--- src/components/newBook/NewBook.jsx
+++ src/components/newBook/NewBook.jsx
@@ -1,156 +1,203 @@
-import { useEffect, useState } from "react";
+import { useReducer } from "react";
 import { Button, Card, Col, Form, Row } from "react-bootstrap";
 import PropTypes from "prop-types";
 
+const initialBookForm = {
+  id: 0,
+  title: "",
+  author: "",
+  rating: "",
+  pageCount: "",
+  imageUrl: "",
+  formValid: false,
+};
+
+const bookFormReducer = (state, action) => {
+  switch (action.type) {
+    case "TITLE_UPDATED":
+      return {
+        ...state,
+        title: action.value,
+        formValid:
+          action.value && state.author && state.rating && state.pageCount,
+      };
+
+    case "AUTHOR_UPDATED":
+      return {
+        ...state,
+        author: action.value,
+        formValid:
+          action.value && state.title && state.rating && state.pageCount,
+      };
+    case "RATING_UPDATED":
+      return {
+        ...state,
+        rating: action.value,
+        formValid:
+          action.value && state.author && state.title && state.pageCount,
+      };
+    case "PAGECOUNT_UPDATED":
+      return {
+        ...state,
+        pageCount: action.value,
+        formValid: action.value && state.author && state.rating && state.title,
+      };
+    case "IMAGEURL_UPDATED":
+      return {
+        ...state,
+        imageUrl: action.value,
+        formValid: state.title && state.author && state.rating && state.title,
+      };
+    case "RESET_FORM":
+      return {
+        ...initialBookForm,
+        formValid: false,
+      };
+    default:
+      return state;
+  }
+};
+
 const NewBook = ({ onBookDataSaved }) => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-  const [enteredRating, setEnteredRating] = useState("");
-  const [enteredPageCount, setEnteredPageCount] = useState("");
-  const [enteredImageUrl, setEnteredImageUrl] = useState("");
-  const [formValid, setFormValid] = useState(false);
-  const [showForm, setShowForm] = useState(false);
+  const [bookForm, dispatch] = useReducer(bookFormReducer, initialBookForm);
 
-  useEffect(() => {
-    setFormValid(
-      enteredTitle !== "" &&
-        enteredAuthor !== "" &&
-        enteredPageCount !== "" &&
-        enteredRating !== ""
-    );
-  }, [enteredTitle, enteredAuthor, enteredPageCount, enteredRating]);
-
-  const handleChangeTitle = (e) => {
-    setEnteredTitle(e.target.value);
+  const changeTitleHandler = (event) => {
+    dispatch({
+      type: "TITLE_UPDATED",
+      value: event.target.value,
+    });
   };
 
   const changeAuthorHandler = (event) => {
-    setEnteredAuthor(event.target.value);
+    dispatch({
+      type: "AUTHOR_UPDATED",
+      value: event.target.value,
+    });
   };
 
   const changeRatingHandler = (event) => {
-    setEnteredRating(event.target.value);
+    dispatch({
+      type: "RATING_UPDATED",
+      value: event.target.value,
+    });
   };
 
   const changePageCountHandler = (event) => {
-    setEnteredPageCount(event.target.value);
+    dispatch({
+      type: "PAGECOUNT_UPDATED",
+      value: event.target.value,
+    });
   };
 
   const changeImageUrlHandler = (event) => {
-    setEnteredImageUrl(event.target.value);
+    dispatch({
+      type: "IMAGEURL_UPDATED",
+      value: event.target.value,
+    });
   };
 
   const submitBookHandler = (event) => {
     event.preventDefault();
     const bookDto = {
       id: 0,
-      bookTitle: enteredTitle,
-      bookAuthor: enteredAuthor,
-      bookRating: enteredRating !== "" ? parseInt(enteredRating, 10) : 0,
-      pageCount: parseInt(enteredPageCount, 10),
-      imageUrl: enteredImageUrl,
+      bookTitle: bookForm.title,
+      bookAuthor: bookForm.author,
+      bookRating: bookForm.rating !== "" ? parseInt(bookForm.rating, 10) : 0,
+      pageCount: parseInt(bookForm.pageCount, 10),
+      imageUrl: bookForm.imageUrl,
     };
-
     onBookDataSaved(bookDto);
-    setEnteredTitle("");
-    setEnteredAuthor("");
-    setEnteredRating("");
-    setEnteredPageCount("");
-    setEnteredImageUrl("");
-  };
-
-  const handleClick = () => {
-    setShowForm(!showForm);
+    dispatch({
+      type: "RESET_FORM",
+    });
   };
 
   return (
-    <>
-      <Button onClick={handleClick}>
-        {showForm ? "Esconder" : "Agregar libro"}
-      </Button>
-      {showForm && (
-        <Card className="m-4 w-50" bg="success">
-          <Card.Body>
-            <Form className="text-white" onSubmit={submitBookHandler}>
-              <Row>
-                <Col md={6}>
-                  <Form.Group className="mb-3" controlId="bookTitle">
-                    <Form.Label>Título</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Ingresar título"
-                      onChange={handleChangeTitle}
-                      value={enteredTitle}
-                    />
-                  </Form.Group>
-                </Col>
-                <Col md={6}>
-                  <Form.Group className="mb-3" controlId="bookAuthor">
-                    <Form.Label>Autor</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Ingresar autor"
-                      onChange={changeAuthorHandler}
-                      value={enteredAuthor}
-                    />
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col md={6}>
-                  <Form.Group className="mb-3" controlId="bookRating">
-                    <Form.Label>Puntuación</Form.Label>
-                    <Form.Control
-                      type="number"
-                      placeholder="Ingresar cantidad de estrellas"
-                      max={5}
-                      min={0}
-                      onChange={changeRatingHandler}
-                      value={enteredRating}
-                    />
-                  </Form.Group>
-                </Col>
-                <Col md={6}>
-                  <Form.Group className="mb-3" controlId="bookPageCount">
-                    <Form.Label>Cantidad de páginas</Form.Label>
-                    <Form.Control
-                      type="number"
-                      placeholder="Ingresar cantidad de páginas"
-                      min={1}
-                      onChange={changePageCountHandler}
-                      value={enteredPageCount}
-                    />
-                  </Form.Group>
-                </Col>
-              </Row>
-              <Row className="justify-content-between">
-                <Form.Group className="mb-3" controlId="bookImageUrl">
-                  <Form.Label>URL de imagen</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Ingresar url de imagen"
-                    onChange={changeImageUrlHandler}
-                    value={enteredImageUrl}
-                  />
-                </Form.Group>
-              </Row>
-              <Row className="justify-content-end">
-                <Col md={3} className="d-flex justify-content-end">
-                  <Button variant="primary" type="submit" disabled={!formValid}>
-                    Agregar lectura
-                  </Button>
-                </Col>
-              </Row>
-            </Form>
-          </Card.Body>
-        </Card>
-      )}
-    </>
+    <Card className="m-4 w-50" bg="success">
+      <Card.Body>
+        <Form className="text-white" onSubmit={submitBookHandler}>
+          <Row>
+            <Col md={6}>
+              <Form.Group className="mb-3" controlId="bookTitle">
+                <Form.Label>Título</Form.Label>
+                <Form.Control
+                  type="text"
+                  required
+                  placeholder="Ingresar título"
+                  onChange={changeTitleHandler}
+                  value={bookForm.title}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group className="mb-3" controlId="bookAuthor">
+                <Form.Label>Autor</Form.Label>
+                <Form.Control
+                  onChange={changeAuthorHandler}
+                  type="text"
+                  placeholder="Ingresar autor"
+                  value={bookForm.author}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <Form.Group className="mb-3" controlId="bookRating">
+                <Form.Label>Puntuación</Form.Label>
+                <Form.Control
+                  type="number"
+                  onChange={changeRatingHandler}
+                  placeholder="Ingresar cantidad de estrellas"
+                  value={bookForm.rating}
+                  max={5}
+                  min={0}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group className="mb-3" controlId="bookPageCount">
+                <Form.Label>Cantidad de páginas</Form.Label>
+                <Form.Control
+                  onChange={changePageCountHandler}
+                  type="number"
+                  placeholder="Ingresar cantidad de páginas"
+                  value={bookForm.pageCount}
+                  min={1}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="justify-content-between">
+            <Form.Group className="mb-3" controlId="bookImageUrl">
+              <Form.Label>URL de imagen</Form.Label>
+              <Form.Control
+                onChange={changeImageUrlHandler}
+                value={bookForm.imageUrl}
+                type="text"
+                placeholder="Ingresar url de imagen"
+              />
+            </Form.Group>
+          </Row>
+          <Row className="justify-content-end">
+            <Col md={3} className="d-flex justify-content-end">
+              <Button
+                disabled={!bookForm.formValid}
+                variant="primary"
+                type="submit"
+              >
+                Agregar lectura
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Card.Body>
+    </Card>
   );
 };
 
 NewBook.propTypes = {
-  onBookDataSaved: PropTypes.func.isRequired,
+  onBookDataSaved: PropTypes.func,
 };
 
 export default NewBook;
